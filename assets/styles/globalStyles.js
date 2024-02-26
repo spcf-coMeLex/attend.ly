66ee3b9367a24bebb45d922406490f4d1fff4a11
melmatx <melpalana13@gmail.com>
2024-02-27 01:26:47 +0800

    Add globalStyles and textStyles

---
+++ assets/styles/globalStyles.js
@@ -0,0 +1,45 @@
+import { StatusBar, StyleSheet } from "react-native";
+
+export const sizes = {
+  xsmall: 3,
+  small: 5,
+  medium: 10,
+  large: 15,
+  xlarge: 20,
+  xxlarge: 30,
+};
+
+export const colors = {
+  primary: "#00539CFF",
+  secondary: "#EEA47FFF",
+  tertiary: "#192F6A",
+};
+
+const globalStyles = StyleSheet.create({
+  flexFull: {
+    flex: 1,
+  },
+  flexCenter: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  center: {
+    alignItems: "center",
+  },
+  rowCenter: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  spaceBetween: {
+    justifyContent: "space-between",
+  },
+  androidPadding: {
+    paddingTop: StatusBar.currentHeight,
+  },
+});
+
+export default globalStyles;
---
+++ assets/styles/textStyles.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+
+const textStyles = StyleSheet.create({
+  logo: {
+    fontFamily: "Lexend-SemiBold",
+    fontSize: 50,
+  },
+  heading: {
+    fontFamily: "Lexend-SemiBold",
+    fontSize: 30,
+    includeFontPadding: false,
+  },
+  subHeading: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 24,
+  },
+  title: {
+    fontFamily: "Lexend-SemiBold",
+    fontSize: 20,
+  },
+  subTitle: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 16,
+  },
+  body: {
+    fontFamily: "Lexend-Regular",
+    fontSize: 14,
+  },
+  caption: {
+    fontFamily: "Lexend-Regular",
+    color: "gray",
+  },
+  link: {
+    fontFamily: "Lexend-Regular",
+    textDecorationLine: "underline",
+    fontSize: 16,
+  },
+  icon: {
+    fontSize: 80,
+  },
+  iconRow: {
+    fontSize: 50,
+    color: "gray",
+    fontFamily: "Lexend-Regular",
+  },
+});
+
+export default textStyles;
