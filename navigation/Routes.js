5afe23b0aaa0688fa125443a99187f8a91c7bd46
melmatx <melpalana13@gmail.com>
2024-02-27 01:31:06 +0800

    Add Routes.js file with navigation routes

---
+++ navigation/Routes.js
@@ -0,0 +1,16 @@
+export default {
+  MAIN: "Main",
+  LOGIN: "Login",
+  SELECT_ROLE: "Select Role",
+  SCAN_CODE: "Scan Code",
+  SIGN_UP: "Sign Up",
+  HOME: "Home",
+  PROFILE: "Profile",
+  FEED: "Feed",
+  DASHBOARD: "Dashboard",
+  CLASS_LIST: "Class List",
+  STUDENT_LIST: "Student List",
+  ADD_CLASS: "Add Class",
+  ADD_STUDENT: "Add Student",
+  EARN: "Earn",
+};
