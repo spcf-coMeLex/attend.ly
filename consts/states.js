f4d703c86fd9a24cda23fa0975614c804452c7f3
melmatx <melpalana13@gmail.com>
2024-02-27 01:28:18 +0800

    Add STATES constant for form fields

---
+++ consts/states.js
@@ -0,0 +1,13 @@
+const STATES = {
+  FIRST_NAME: "firstName",
+  MIDDLE_NAME: "middleName",
+  LAST_NAME: "lastName",
+  GENDER: "gender",
+  BIRTHDAY: "birthday",
+  ADDRESS: "address",
+  PARENTS_EMAIL: "parentsEmail",
+  PASSWORD: "password",
+  CONFIRM_PASSWORD: "confirmPassword",
+};
+
+export default STATES;
