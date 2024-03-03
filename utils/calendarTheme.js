7fce5a4a66fab4876e005d0ea72d39f73fc471d5
melmatx <melpalana13@gmail.com>
2024-03-03 22:31:49 +0800

    Add calendar theme file

---
+++ utils/calendarTheme.js
@@ -0,0 +1,16 @@
+import { colors } from "../assets/styles/globalStyles";
+
+const calendarTheme = {
+  dotStyle: { width: 10, height: 10, borderRadius: 20 },
+  dayTextColor: "black",
+  monthTextColor: "black",
+  textMonthFontFamily: "Lexend-Regular",
+  textMonthFontSize: 20,
+  textMonthFontWeight: "bold",
+  textDayFontWeight: "400",
+  textSectionTitleColor: "black",
+  todayTextColor: colors.primary,
+  arrowColor: colors.primary,
+};
+
+export default calendarTheme;
