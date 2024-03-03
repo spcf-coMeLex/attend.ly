2258259384e49b6bd3d92de41f9e97cfcacded22
melmatx <melpalana13@gmail.com>
2024-03-03 22:31:32 +0800

    Add getCoordinates utility function

---
+++ utils/getCoordinates.js
@@ -0,0 +1,20 @@
+import { URL } from "react-native-url-polyfill";
+
+const getCoordinates = async (place) => {
+  const geoCodeUrl = new URL("https://geocode.maps.co/search");
+
+  geoCodeUrl.searchParams.append("q", place);
+  geoCodeUrl.searchParams.append(
+    "api_key",
+    process.env.EXPO_PUBLIC_GEOCODING_API_KEY,
+  );
+
+  const response = await fetch(geoCodeUrl);
+  const data = await response.json();
+  const result = data[0]; // Assuming the first result is the correct one
+  const { lat: latitude, lon: longitude } = result; // Get the latitude and longitude from the result
+
+  return { longitude, latitude };
+};
+
+export default getCoordinates;
