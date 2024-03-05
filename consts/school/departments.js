ae634165e3b093fcbf5784b4e80735f5af4d51ea
melmatx <melpalana13@gmail.com>
2024-03-05 21:26:34 +0800

    Add departmentData constant

---
+++ consts/school/departments.js
@@ -0,0 +1,11 @@
+const departmentData = [
+  { label: "CCIS", value: "CCIS" },
+  { label: "COB", value: "COB" },
+  { label: "COE", value: "COE" },
+  { label: "CAS", value: "CAS" },
+  { label: "CHM", value: "CHM" },
+  { label: "COC", value: "COC" },
+  { label: "COED", value: "COED" },
+];
+
+export default departmentData;
