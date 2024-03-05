bab9ace7258ad5da63c3d1a6817cc23ba23502c7
melmatx <melpalana13@gmail.com>
2024-03-05 21:22:36 +0800

    Refactor MainNavigation component to include new screens and options

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,25 +1,34 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Platform, Text } from "react-native";
+import { useShallow } from "zustand/react/shallow";
 
 import BottomTabs from "./BottomTabs";
 import Routes from "./Routes";
 import { colors } from "../assets/styles/globalStyles";
 import textStyles from "../assets/styles/textStyles";
-import AddClass from "../screens/AddClass";
-import AddStudent from "../screens/AddStudent";
+import ROLES from "../consts/roles";
+import AddSection from "../screens/AddSection";
 import Login from "../screens/Login";
 import Profile from "../screens/Profile";
+import Rewards from "../screens/Rewards";
 import ScanCode from "../screens/ScanCode";
 import SelectRole from "../screens/SelectRole";
 import SignUp from "../screens/SignUp";
 import StudentList from "../screens/StudentList";
+import StudentProfile from "../screens/StudentProfile";
+import SubjectList from "../screens/SubjectList";
 import useAuthStore from "../stores/useAuthStore";
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigation = () => {
-  const identity = useAuthStore((state) => state.identity);
-  const isRegistered = useAuthStore((state) => state.isRegistered);
+  const { identity, isRegistered, role } = useAuthStore(
+    useShallow((state) => ({
+      identity: state.identity,
+      isRegistered: state.isRegistered,
+      role: state.role,
+    })),
+  );
 
   return (
     <Stack.Navigator
@@ -42,7 +51,11 @@ const MainNavigation = () => {
       {identity && !isRegistered ? (
         <>
           <Stack.Screen name={Routes.SELECT_ROLE} component={SelectRole} />
-          <Stack.Screen name={Routes.SCAN_CODE} component={ScanCode} />
+          <Stack.Screen
+            name={Routes.SCAN_CODE}
+            component={ScanCode}
+            initialParams={{ stackView: true }}
+          />
           <Stack.Screen name={Routes.SIGN_UP} component={SignUp} />
         </>
       ) : null}
@@ -68,12 +81,43 @@ const MainNavigation = () => {
           >
             <Stack.Screen name={Routes.PROFILE} component={Profile} />
             <Stack.Screen
-              name={Routes.STUDENT_LIST}
-              component={StudentList}
-              options={({ route }) => ({ title: route.params.className })}
+              name={Routes.STUDENT_PROFILE}
+              component={StudentProfile}
+              options={({ route }) => {
+                const { studentName } = route.params;
+
+                if (studentName.length > 20) {
+                  return {
+                    title: studentName.substring(0, 10) + "...",
+                  };
+                }
+
+                return { title: studentName };
+              }}
             />
-            <Stack.Screen name={Routes.ADD_CLASS} component={AddClass} />
-            <Stack.Screen name={Routes.ADD_STUDENT} component={AddStudent} />
+
+            {role === ROLES.TEACHER && (
+              <>
+                <Stack.Screen
+                  name={Routes.ADD_SECTION}
+                  component={AddSection}
+                />
+                <Stack.Screen
+                  name={Routes.SUBJECT_LIST}
+                  component={SubjectList}
+                  options={({ route }) => ({ title: route.params.className })}
+                />
+                <Stack.Screen
+                  name={Routes.STUDENT_LIST}
+                  component={StudentList}
+                  options={({ route }) => ({ title: route.params.subjectName })}
+                />
+              </>
+            )}
+
+            {role === ROLES.STUDENT && (
+              <Stack.Screen name={Routes.REWARDS} component={Rewards} />
+            )}
           </Stack.Group>
         </>
       ) : null}
