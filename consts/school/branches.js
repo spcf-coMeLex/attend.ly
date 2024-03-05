069cc817647eb7537ed88339112c36c5238dc3ba
melmatx <melpalana13@gmail.com>
2024-03-05 21:26:13 +0800

    Add school branches data

---
+++ consts/school/branches.js
@@ -0,0 +1,9 @@
+const branchData = [
+  { label: "Balibago", value: "BALIBAGO" },
+  { label: "Miranda", value: "MIRANDA" },
+  { label: "Caloocan", value: "CALOOCAN" },
+  { label: "San Fernando", value: "SAN FERNANDO" },
+  { label: "Rizal", value: "RIZAL" },
+];
+
+export default branchData;
